cc9a32cb0b05e3fa9ee1eb5bb6013730f833dd59
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-04 16:17:13 -0700

    update project with group work including SplitString and ReverseString tests

--- PalindromeChecker.Solution/Palindrome.Tests/Models/PalindromeTests.cs
+++ PalindromeChecker.Solution/Palindrome.Tests/Models/PalindromeTests.cs
@@ -6,11 +6,29 @@ namespace PalindromeChecker.Tests
   [TestClass]
   public class PalindromeTests
   {
-    public void GatherInput_GatherInputFromUser_UserInput()
+    [TestMethod]
+    public void SplitString_TakesAndSplitsAString_True()
     {
-    Palindrome testPalindrome = new Palindrome();
-    string userInput = "racecar";
-    Assert.AreEqual(string userInput, testPalindrome.GatherInput(userInput));
+      string testString1 = "racecar";
+      char[] testString2 = { 'r', 'a', 'c', 'e', 'c', 'a', 'r' };
+      Palindrome2 testPalindrome = new Palindrome2();
+      CollectionAssert.AreEqual(testString2, testPalindrome.SplitString(testString1));
     }
+        [TestMethod]
+    public char ReverseString_TakesSplitStringAndReversesIt_True()
+    {
+      string testString1 = "epicodus";
+      char[] testString2 = { "e", "p", "i", "c", "o", "d", "u", "s" };
+      char[] reversedString =  { "s", "u", "d", "o", "c", "i", "p", "e" };
+      Palindrome2 testPalindrome = new Palindrome2();
+      CollectionAssert.AreNotEqual(reversedString, testPalindrome.ReverseString(testString2));
+    }
+    
+    
+
+      //// take string 
+      //// split string up 
+      //// reverse string 
+      //// compare first string with reversed string
   }
 }
