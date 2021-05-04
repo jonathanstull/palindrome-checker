192c30082e7d50a978c7cc246c5a4d56f0d75f7f
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-05-04 12:49:48 -0700

    add first test, GatherInput

--- PalindromeChecker.Solution/Palindrome.Tests/Models/PalindromeTests.cs
+++ PalindromeChecker.Solution/Palindrome.Tests/Models/PalindromeTests.cs
@@ -6,6 +6,11 @@ namespace PalindromeChecker.Tests
   [TestClass]
   public class PalindromeTests
   {
-    
+    public void GatherInput_GatherInputFromUser_UserInput()
+    {
+    Palindrome testPalindrome = new Palindrome();
+    string userInput = "racecar";
+    Assert.AreEqual(string userInput, testPalindrome.GatherInput(userInput));
+    }
   }
 }
